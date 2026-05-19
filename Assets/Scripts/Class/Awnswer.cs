529c00efc7497aa920c9c96378051dbd7574eac8
August <goosey9999@gmail.com>
2026-05-19 15:55:08 -0400

    classsssss

---
+++ Assets/Scripts/Class/Awnswer.cs
@@ -0,0 +1,11 @@
+using System.Net.Http.Headers;
+using UnityEngine;
+
+
+[System.Serializable]
+public class Awnswer 
+{
+   public string text;
+    public bool isCorrect;
+
+}
---
+++ Assets/Scripts/Class/Question.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public class Question 
+{
+
+    public string questionText;
+    public List<Awnswer> awnsers;
+
+}
